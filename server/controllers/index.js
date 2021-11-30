52286ca454d0e266bcba34c3960164fc2f77c18a
jhpark9007 <jhpark9007@gmail.com>
2021-11-30 10:02:32 +0900

    Feat: 서버 초기 설정

---
+++ server/controllers/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  user: {
+    login: require('./user/login'),
+    logout: require('./user/logout'),
+    signup: require('./user/signup'),
+  },
+  mypage: {},
+  room: {},
+  roomDetail: {},
+  map: {},
+};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,40 @@
+require('dotenv').config();
+const fs = require('fs');
+const https = require('https');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 const express = require('express');
 const app = express();
-const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: ['https://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  }),
+);
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// router
+
+const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
+
+let httpsServer;
+if (fs.existsSync('./key.pem') && fs.existsSync('./cert.pem')) {
+  const privateKey = fs.readFileSync(__dirname + '/key.pem', 'utf8');
+  const certificate = fs.readFileSync(__dirname + '/cert.pem', 'utf8');
+  const credentials = { key: privateKey, cert: certificate };
+
+  httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(HTTPS_PORT, () => console.log('server runnning'));
+} else {
+  httpsServer = app.listen(HTTPS_PORT);
+}
+module.exports = httpsServer;
